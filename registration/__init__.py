28c372ec8676da32397923a88b7feeed1bbd5bf2
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2012-01-29 15:42:11 +0100

    Fix version computation.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polyconseil.fr>

--- registration/__init__.py
+++ registration/__init__.py
@@ -6,11 +6,10 @@ def get_version():
         version = '%s.%s' % (version, VERSION[2])
     if VERSION[3:] == ('alpha', 0):
         version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = "%s %s" % (version, VERSION[3])
-            if VERSION[4] != 0:
-                version = '%s %s' % (version, VERSION[4])
+    elif VERSION[3] != 'final':
+        version = "%s %s" % (version, VERSION[3])
+        if VERSION[4] != 0:
+            version = '%s %s' % (version, VERSION[4])
     return version
 
 from django.conf import settings
